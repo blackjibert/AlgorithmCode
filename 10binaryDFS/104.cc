ab0d039aebaf7d821efcecc5f11cd1e2ce257a2c
BlackJibert <z1319885580@163.com>
2023-11-28 22:31:13 +0800

    二叉树深度遍历

--- 10binaryDFS/104.cc
+++ 10binaryDFS/104.cc
@@ -3,54 +3,56 @@
 #include <queue>
 #include "include/treenode.h"
 
-class Solution 
+class Solution
 {
 public:
-    //递归方法
-    int maxDepthRecursion(TreeNode* root)
+    // 递归方法
+    int maxDepthRecursion(TreeNode *root)
     {
-        if (root == nullptr) 
+        if (root == nullptr)
         {
             return 0;
         }
         return std::max(maxDepthRecursion(root->left), maxDepthRecursion(root->right)) + 1;
     }
-    //非递归实现, 每一层进行遍历
-    int maxDepth(TreeNode* root)
+    // 非递归实现, 每一层进行遍历
+    int maxDepth(TreeNode *root)
     {
-        if(root == nullptr) return 0;
-        int num=0;
-        std::queue<TreeNode*> que;
+        if (root == nullptr)
+            return 0;
+        int num = 0;
+        std::queue<TreeNode *> que;
         que.push(root);
-        while (!que.empty()) 
+        while (!que.empty())
         {
-            int n=que.size();
-            for(int i=0;i<n;i++)
+            int n = que.size();
+            for (int i = 0; i < n; i++)
             {
-                TreeNode* curr = que.front();
-                if (curr->left) que.push(curr->left);
-                if (curr->right) que.push(curr->right);
+                TreeNode *curr = que.front();
+                if (curr->left)
+                    que.push(curr->left);
+                if (curr->right)
+                    que.push(curr->right);
                 que.pop();
             }
             num++;
         }
         return num;
     }
-
 };
 
-int main() 
+int main()
 {
     // 创建二叉树
-    TreeNode* root = new TreeNode(1);
+    TreeNode *root = new TreeNode(1);
     root->left = new TreeNode(2);
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
     root->left->right = new TreeNode(5);
- 
+
     Solution ss;
     int maxDepth = ss.maxDepthRecursion(root);
-    std::cout<<maxDepth<<std::endl;
+    std::cout << maxDepth << std::endl;
     // 释放内存
     delete root->left->left;
     delete root->left->right;
--- 10binaryDFS/872.cc
+++ 10binaryDFS/872.cc
@@ -4,35 +4,36 @@
 #include <vector>
 #include "include/treenode.h"
 
-class Solution 
+class Solution
 {
 public:
-   //递归收集叶值序列
-   void collectLeaves(TreeNode* root, std::vector<int>& leaves)
-   {
-       if(!root) return;
-       if(!root->left && !root->right)
-       {
-           leaves.push_back(root->val);
-           return;
-       }
+    // 递归收集叶值序列
+    void collectLeaves(TreeNode *root, std::vector<int> &leaves)
+    {
+        if (!root)
+            return;
+        if (!root->left && !root->right)
+        {
+            leaves.push_back(root->val);
+            return;
+        }
         collectLeaves(root->left, leaves);
         collectLeaves(root->right, leaves);
-   }
+    }
 
-   bool leafSimilar(TreeNode* root1, TreeNode* root2)
-   {
-       std::vector<int> leaves1, leaves2;
-       collectLeaves(root1, leaves1);
-       collectLeaves(root2, leaves2);
-       return leaves1 == leaves2;
-   }
+    bool leafSimilar(TreeNode *root1, TreeNode *root2)
+    {
+        std::vector<int> leaves1, leaves2;
+        collectLeaves(root1, leaves1);
+        collectLeaves(root2, leaves2);
+        return leaves1 == leaves2;
+    }
 };
 
-int main() 
+int main()
 {
-     // 创建两棵二叉树
-    TreeNode* root1 = new TreeNode(3);
+    // 创建两棵二叉树
+    TreeNode *root1 = new TreeNode(3);
     root1->left = new TreeNode(5);
     root1->right = new TreeNode(1);
     root1->left->left = new TreeNode(6);
@@ -42,7 +43,7 @@ int main()
     root1->left->right->left = new TreeNode(7);
     root1->left->right->right = new TreeNode(4);
 
-    TreeNode* root2 = new TreeNode(3);
+    TreeNode *root2 = new TreeNode(3);
     root2->left = new TreeNode(5);
     root2->right = new TreeNode(1);
     root2->left->left = new TreeNode(6);
@@ -51,12 +52,12 @@ int main()
     root2->right->right = new TreeNode(2);
     root2->right->right->left = new TreeNode(9);
     root2->right->right->right = new TreeNode(8);
- 
+
     Solution ss;
     bool maxDepth = ss.leafSimilar(root1, root2);
-    std::cout<<maxDepth<<std::endl;
-    
+    std::cout << maxDepth << std::endl;
+
     // 释放内存
-    
+
     return 0;
 }
