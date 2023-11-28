593f979fc5f49ccbe041a3b0cbd65617bce2a61e
BlackJibert <z1319885580@163.com>
2023-11-28 22:34:51 +0800

    滑动窗口

--- 3sliding_window/1456.cc
+++ 3sliding_window/1456.cc
@@ -5,42 +5,43 @@
 #include <algorithm>
 class Solution
 {
-   public:
-      int maxVowels(std::string s, int k)
+public:
+   int maxVowels(std::string s, int k)
+   {
+      // 双指针法, 滑动窗口, 快慢指针
+      int maxVow = INT16_MIN;
+      int mySize = 0; // 最大元音字母数
+      int j = 0;
+      for (int i = 0; i < s.length(); i++) // string类的size和length没有区别,都是返回字节数
       {
-         //双指针法, 滑动窗口, 快慢指针
-         int maxVow = INT16_MIN;
-         int mySize=0;//最大元音字母数
-         int j=0;
-         for(int i=0;i<s.length();i++) //string类的size和length没有区别,都是返回字节数
+         if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
          {
-            if(s[i] =='a' || s[i] == 'e' || s[i] =='i' || s[i] =='o' || s[i] =='u')
+            mySize++;
+         }
+         if (i - j + 1 == k)
+         {
+            if (mySize > maxVow)
+               maxVow = mySize;
+            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
             {
-               mySize++;
+               mySize--;
+               j++;
             }
-            if(i-j+1 == k)
+            else
             {
-               if(mySize>maxVow) maxVow = mySize;
-               if(s[j] =='a' || s[j] == 'e' || s[j] =='i' || s[j] =='o' || s[j] =='u')
-               {
-                  mySize --;
-                  j++;
-               }
-               else 
-               {
-                  j++;
-               }
-
+               j++;
             }
          }
-         return maxVow;
       }
+      return maxVow;
+   }
 };
-int main() {
+int main()
+{
    Solution ss;
-   std::string s ="leetcode"; 
-   int k= 4;
-   int res = ss.maxVowels(s,k);
-   std::cout<<res<<std::endl;
+   std::string s = "leetcode";
+   int k = 4;
+   int res = ss.maxVowels(s, k);
+   std::cout << res << std::endl;
    return 0;
 }
--- 3sliding_window/643.cc
+++ 3sliding_window/643.cc
@@ -5,30 +5,31 @@
 #include <algorithm>
 class Solution
 {
-   public:
-      double findMaxAverate(std::vector<int>& nums, int k)
+public:
+   double findMaxAverate(std::vector<int> &nums, int k)
+   {
+      int sum = 0, maxi = INT16_MIN;
+      for (int i = 0; i < nums.size(); i++)
       {
-         int sum=0,maxi=INT16_MIN;
-         for(int i=0;i<nums.size();i++)
+         sum += nums[i];
+         if (i >= k)
          {
-            sum+=nums[i];
-            if(i>=k)
-            {
-               sum-=nums[i-k];
-            }
-            if(i+1>k)
-            {
-               maxi=std::max(sum, maxi);
-            }
+            sum -= nums[i - k];
          }
-         return maxi/(double)k;
+         if (i + 1 > k)
+         {
+            maxi = std::max(sum, maxi);
+         }
       }
+      return maxi / (double)k;
+   }
 };
-int main() {
+int main()
+{
    Solution ss;
-   std::vector<int> s ={1,12,-5,-6,50,3};
-   int k= 4;
-   double res = ss.findMaxAverate(s,k);
-   std::cout<<res<<std::endl;
+   std::vector<int> s = {1, 12, -5, -6, 50, 3};
+   int k = 4;
+   double res = ss.findMaxAverate(s, k);
+   std::cout << res << std::endl;
    return 0;
 }
