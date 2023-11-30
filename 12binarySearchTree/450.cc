09c0eb34a95330898f90dd6526ab5e59eddcf99b
BlackJibert <z1319885580@163.com>
2023-11-30 22:41:51 +0800

    13图遍历方式

--- 12binarySearchTree/450.cc
+++ 12binarySearchTree/450.cc
@@ -15,28 +15,28 @@ public:
         if(!root) return root;
         if(root->val == key) 
         {
-            // 第2种情况: 左右孩子都为空(叶子节点)，直接删除节点， 返回NULL为根节点
+            // 第2种情况: 左右节点都为空(叶子节点), 直接删除节点, 返回NULL为根节点
             if(!root->left && !root->right)
             {
                 delete root;
                 return nullptr;
             }
-            // 第3种情况: 其左孩子为空，右孩子不为空，删除节点，右孩子补位 ，返回右孩子为根节点
+            // 第3种情况: 其左节点为空,  右节点不为空, 删除节点, 右节点补位, 返回右节点为根节点
             else if(!root->left && root->right)
             {
                 TreeNode* retNode = root->right;
                 delete root;
                 return retNode;
             }
-            // 第4种情况: 其右孩子为空, 左孩子不为空, 删除节点, 左孩子补位, 返回左孩子为根节点
+            // 第4种情况: 其右节点为空, 左节点不为空, 删除节点, 左节点补位, 返回左节点为根节点
             else if(!root->right && root->left)
             {
                 TreeNode* retNode = root->left;
                 delete root;
                 return retNode;
             }
-            // 第5种情况：左右孩子节点都不为空，则将删除节点的左子树放到删除节点的右子树的最左面节点的左孩子的位置
-            // 并返回删除节点右孩子为新的根节点。
+            // 第5种情况：左右节点都不为空, 则将删除节点的左节点放到删除节点的右节点的最左面节点的左节点的位置
+            // 并返回删除节点, 右节点为新的根节点。
             else 
             {
                 TreeNode* cur = root->right;
